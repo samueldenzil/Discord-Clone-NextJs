593879bb72b60aa66ebb0a0f2fdbf2aa4312df5a
samueldenzil <denzil.s.samuel@gmail.com>
2023-09-04 15:54:15 +0530

    Add lint-staged for setting up Prettier and ESLint with pre-commit hook

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,5 @@
-"use client";
-import AuthForm from "../components/AuthForm";
+'use client'
+import AuthForm from '../components/AuthForm'
 
 export default function Home() {
   return (
@@ -7,13 +7,11 @@ export default function Home() {
       <div className="p-8 bg-gray-800 rounded-md shadow-md">
         <div className="flex space-y-2 flex-col text-center cursor-default">
           <h3 className="text-gray-200 font-medium text-xl">Welcome Back</h3>
-          <p className="text-gray-400 text-sm">
-            We're so excited to see you again!
-          </p>
+          <p className="text-gray-400 text-sm">We're so excited to see you again!</p>
         </div>
 
         <AuthForm />
       </div>
     </div>
-  );
+  )
 }
