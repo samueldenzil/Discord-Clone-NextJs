4afcd2f4dcc99b30c996764a0b4bb1a03d460aec
samueldenzil <denzil.s.samuel@gmail.com>
2023-09-05 12:49:51 +0530

    Create Server Sidebar

---
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,42 @@
+import { redirect } from 'next/navigation'
+
+import prisma from '@/lib/db'
+
+import getCurrentUser from '@/actions/getCurrentUser'
+import ServerSidebar from '@/components/server/server-sidebar'
+
+export default async function ServerIdLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: { serverId: string }
+}) {
+  const user = await getCurrentUser()
+
+  if (!user) {
+    redirect('/login')
+  }
+
+  const server = await prisma.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: { userId: user.id },
+      },
+    },
+  })
+
+  if (!server) {
+    return redirect('/')
+  }
+
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+      <main className="md:pl-60 h-full">{children}</main>
+    </div>
+  )
+}
---
+++ components/server/server-sidebar.tsx
@@ -0,0 +1,56 @@
+import { ChannelType } from '@prisma/client'
+import { redirect } from 'next/navigation'
+
+import getCurrentUser from '@/actions/getCurrentUser'
+import prisma from '@/lib/db'
+import ServerHeader from '@/components/server/server-header'
+
+type ServerSidebarProps = {
+  serverId: string
+}
+
+export default async function ServerSidebar({ serverId }: ServerSidebarProps) {
+  const user = await getCurrentUser()
+
+  if (!user) {
+    redirect('/')
+  }
+
+  const server = await prisma.server.findUnique({
+    where: {
+      id: serverId,
+    },
+    include: {
+      channels: {
+        orderBy: {
+          createdAt: 'asc',
+        },
+      },
+      members: {
+        include: {
+          user: true,
+        },
+        orderBy: {
+          role: 'asc',
+        },
+      },
+    },
+  })
+
+  if (!server) {
+    redirect('/')
+  }
+
+  const textChannels = server.channels.filter((channel) => channel.type === ChannelType.TEXT)
+  const audioChannels = server.channels.filter((channel) => channel.type === ChannelType.AUDIO)
+  const videoChannels = server.channels.filter((channel) => channel.type === ChannelType.VIDEO)
+  const members = server.members.filter((member) => member.id !== user.id)
+
+  const role = server.members.find((member) => member.userId === user.id)?.role
+
+  return (
+    <div className="h-full w-full flex flex-col bg-[#f2f3f5] dark:bg-[#2b2d31] text-primary">
+      <ServerHeader server={server} role={role} />
+    </div>
+  )
+}
