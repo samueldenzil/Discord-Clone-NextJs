b7a8eb9e5299fa5d0f05af430bdbaddf7146c0ab
samueldenzil <denzil.s.samuel@gmail.com>
2023-09-09 17:27:32 +0530

    Create Search Server Modal

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,12 +1,27 @@
-import { ChannelType } from '@prisma/client'
+import { ChannelType, MemberRole } from '@prisma/client'
 import { redirect } from 'next/navigation'
+import { Hash, Mic, Shield, ShieldAlert, ShieldCheck, Video } from 'lucide-react'
 
+import { ScrollArea } from '@/components/ui/scroll-area'
 import getCurrentUser from '@/actions/get-current-user'
 import prisma from '@/lib/db'
 import ServerHeader from '@/components/server/server-header'
+import ServerSearch from '@/components/server/server-search'
 
 type ServerSidebarProps = {
   serverId: string
+}
+
+const iconMap = {
+  [ChannelType.TEXT]: <Hash className="h-4 w-4 mr-2" />,
+  [ChannelType.AUDIO]: <Mic className="h-4 w-4 mr-2" />,
+  [ChannelType.VIDEO]: <Video className="h-4 w-4 mr-2" />,
+}
+
+const roleIconMap = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: <ShieldCheck className="h-4 w-4 mr-2 text-indigo-500" />,
+  [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 mr-2 text-rose-500" />,
 }
 
 export default async function ServerSidebar({ serverId }: ServerSidebarProps) {
@@ -41,16 +56,61 @@ export default async function ServerSidebar({ serverId }: ServerSidebarProps) {
     redirect('/')
   }
 
-  const textChannels = server.channels.filter((channel) => channel.type === ChannelType.TEXT)
-  const audioChannels = server.channels.filter((channel) => channel.type === ChannelType.AUDIO)
-  const videoChannels = server.channels.filter((channel) => channel.type === ChannelType.VIDEO)
-  const members = server.members.filter((member) => member.id !== user.id)
+  const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT)
+  const audioChannels = server?.channels.filter((channel) => channel.type === ChannelType.AUDIO)
+  const videoChannels = server?.channels.filter((channel) => channel.type === ChannelType.VIDEO)
+  const members = server?.members.filter((member) => member.userId !== user.id)
 
   const role = server.members.find((member) => member.userId === user.id)?.role
 
   return (
     <div className="h-full w-full flex flex-col bg-[#f2f3f5] dark:bg-[#2b2d31] text-primary">
       <ServerHeader server={server} role={role} />
+
+      <ScrollArea className="flex-1">
+        <div className="mt-2">
+          <ServerSearch
+            data={[
+              {
+                label: 'Text Channels',
+                type: 'channel',
+                data: textChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: 'Voice Channels',
+                type: 'channel',
+                data: audioChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: 'Video Channels',
+                type: 'channel',
+                data: videoChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: 'Members',
+                type: 'member',
+                data: members?.map((member) => ({
+                  id: member.id,
+                  name: member?.user.name,
+                  icon: roleIconMap[member.role],
+                })),
+              },
+            ]}
+          />
+        </div>
+      </ScrollArea>
     </div>
   )
 }
