f8bc73adc1a3fae0711d70416b52c0eb6f7ed37b
samueldenzil <denzil.s.samuel@gmail.com>
2023-09-14 23:07:40 +0530

    Change font from inter to open-sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
 
 import AuthContext from '@/context/AuthContext'
 import ToastContext from '@/context/ToastContext'
@@ -11,7 +11,7 @@ import { SocketProvider } from '@/components/providers/socket-provider'
 
 import { cn } from '@/lib/utils'
 
-const inter = Inter({ subsets: ['latin'] })
+const openSans = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn(inter.className, 'bg-white dark:bg-[#313338]')}>
+      <body className={cn(openSans.className, 'bg-white dark:bg-[#313338]')}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
