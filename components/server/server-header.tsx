3ea46d4bdbdcda9d9b64bd1c4839344377e17231
samueldenzil <denzil.s.samuel@gmail.com>
2023-09-18 11:15:40 +0530

    Update ServerHeader component

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -26,70 +26,76 @@ export default function ServerHeader({ server, role }: ServerHeaderProps) {
   const isModerator = isAdmin || role === MemberRole.MODERATOR
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger className="focus:outline-none" asChild>
-        <button className="w-full font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition-all">
-          {server.name}
-          <ChevronDown className="h-5 w-5 ml-auto" />
-        </button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-2">
-        {isModerator && (
-          <DropdownMenuItem
-            onClick={() => onOpen('invite', { server })}
-            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
-          >
-            Invite People
-            <UserPlus className="h-4 w-4 ml-auto" />
-          </DropdownMenuItem>
-        )}
-        {isAdmin && (
-          <DropdownMenuItem
-            onClick={() => onOpen('editServer', { server })}
-            className="px-3 py-2 text-sm cursor-pointer"
-          >
-            Server Settings
-            <Settings className="h-4 w-4 ml-auto" />
-          </DropdownMenuItem>
-        )}
-        {isAdmin && (
-          <DropdownMenuItem
-            onClick={() => onOpen('members', { server })}
-            className="px-3 py-2 text-sm cursor-pointer"
-          >
-            Manage Members
-            <Users className="h-4 w-4 ml-auto" />
-          </DropdownMenuItem>
-        )}
-        {isModerator && (
-          <DropdownMenuItem
-            onClick={() => onOpen('createChannel', { server })}
-            className="px-3 py-2 text-sm cursor-pointer"
-          >
-            Create Channel
-            <PlusCircle className="h-4 w-4 ml-auto" />
-          </DropdownMenuItem>
-        )}
-        {isModerator && <DropdownMenuSeparator />}
-        {isAdmin && (
-          <DropdownMenuItem
-            onClick={() => onOpen('deleteServer', { server })}
-            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
-          >
-            Delete Server
-            <Trash className="h-4 w-4 ml-auto" />
-          </DropdownMenuItem>
-        )}
-        {!isAdmin && (
-          <DropdownMenuItem
-            onClick={() => onOpen('leaveServer', { server })}
-            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
-          >
-            Leave Server
-            <LogOut className="h-4 w-4 ml-auto" />
-          </DropdownMenuItem>
-        )}
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div
+      style={{ backgroundImage: `url(${server.imageUrl})` }}
+      className="relative h-[135px] bg-cover bg-center border-neutral-200 dark:border-neutral-800 border-b-2"
+    >
+      <DropdownMenu>
+        <DropdownMenuTrigger className="focus:outline-none relative z-10" asChild>
+          <button className="w-full font-semibold px-3 flex items-center h-12 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition-all">
+            {server.name}
+            <ChevronDown className="h-5 w-5 ml-auto" />
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-2">
+          {isModerator && (
+            <DropdownMenuItem
+              onClick={() => onOpen('invite', { server })}
+              className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+            >
+              Invite People
+              <UserPlus className="h-4 w-4 ml-auto" />
+            </DropdownMenuItem>
+          )}
+          {isAdmin && (
+            <DropdownMenuItem
+              onClick={() => onOpen('editServer', { server })}
+              className="px-3 py-2 text-sm cursor-pointer"
+            >
+              Server Settings
+              <Settings className="h-4 w-4 ml-auto" />
+            </DropdownMenuItem>
+          )}
+          {isAdmin && (
+            <DropdownMenuItem
+              onClick={() => onOpen('members', { server })}
+              className="px-3 py-2 text-sm cursor-pointer"
+            >
+              Manage Members
+              <Users className="h-4 w-4 ml-auto" />
+            </DropdownMenuItem>
+          )}
+          {isModerator && (
+            <DropdownMenuItem
+              onClick={() => onOpen('createChannel', { server })}
+              className="px-3 py-2 text-sm cursor-pointer"
+            >
+              Create Channel
+              <PlusCircle className="h-4 w-4 ml-auto" />
+            </DropdownMenuItem>
+          )}
+          {isModerator && <DropdownMenuSeparator />}
+          {isAdmin && (
+            <DropdownMenuItem
+              onClick={() => onOpen('deleteServer', { server })}
+              className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+            >
+              Delete Server
+              <Trash className="h-4 w-4 ml-auto" />
+            </DropdownMenuItem>
+          )}
+          {!isAdmin && (
+            <DropdownMenuItem
+              onClick={() => onOpen('leaveServer', { server })}
+              className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+            >
+              Leave Server
+              <LogOut className="h-4 w-4 ml-auto" />
+            </DropdownMenuItem>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <div className="absolute inset-0 bg-gradient-to-b from-neutral-800 opacity-90" />
+    </div>
   )
 }
