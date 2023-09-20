d6321f7e4876404d4b0c97acca83f37b86b12b2a
samueldenzil <denzil.s.samuel@gmail.com>
2023-09-20 12:46:41 +0530

    Update Home page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,15 @@
 import getServer from '@/lib/get-server'
 import InitialModal from '@/components/modals/initial-modal'
 import { redirect } from 'next/navigation'
+import getCurrentUser from '@/lib/get-current-user'
 
 export default async function Home() {
+  const user = await getCurrentUser()
+
+  if (!user) {
+    redirect('/login')
+  }
+
   const server = await getServer()
 
   if (server) {
