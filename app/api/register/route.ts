c0c689a0141ac2f8525962729939b983d5267a2c
samueldenzil <denzil.s.samuel@gmail.com>
2023-09-24 18:58:16 +0530

    Update api/register/route.ts

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -22,6 +22,7 @@ export async function POST(request: Request) {
         name,
         email,
         hashedPassword,
+        emailVerified: new Date(),
         image: publicUrlData.publicUrl,
       },
     })
